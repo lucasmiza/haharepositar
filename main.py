d5739627f57435bbd0b61752b35d459a09156ff7
lucasmiza <lucka.smizikova@gmail.com>
2024-09-24 09:30:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ def schreder ():
             print (i)
 
 schreder()
+
+#u1
+
+def fun1():
+    for i in range(1,11):
+        print (i)
+
+fun1()
+
+#u2a
+
+def fun2(n):
+    for i in range (1,1+n):
+        print(i)
+
+fun2(20)
+
+#u2b
+
+def fun
+    
+#u3
