0771c8827ea6f8a373789f236ce1974507493052
lucasmiza <lucka.smizikova@gmail.com>
2024-09-24 08:45:33 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ("Ahoj svet")
+def schreder ():
+    for i in range (1000,10000):
+        lastwo = i % 100
+        firstwo = i // 100
+        if firstwo**2 + lastwo**2 == i:
+            print (i)
+
+schreder()
